79138eb1463182ca50f5e30f21087013b63a045e
cooljasonmelton <cooljasonmelton>
2020-08-07 01:22:50 -0500

    set up a fact container and played with css

---
+++ README.md
@@ -0,0 +1,3 @@
+Jason's Awesome Fact Machine
+
+i'm going to have a series of animations that happen as you scroll passed them that display an amazing fact. Going for, idk, five amazing facts. Various animations, pictures? Yeah. Will figure this
--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, {useState} from 'react';
 import './App.css';
 
+// rough notation
 import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
+
+// 
+import Fact1 from './fact-1/Fact1'
 
 
 
 const App = () => {
   return (
     <div className="App" >
+      <Fact1/>
 
 
 
